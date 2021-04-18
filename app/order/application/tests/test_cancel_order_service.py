a3291fa6edec64e3e91e95809b14a77aec12c8b0
amazingguni <amazingguni@gmail.com>
2021-04-18 17:37:10 +0900

    Add testcase for testing already shipped exception

--- app/order/application/tests/test_cancel_order_service.py
+++ app/order/application/tests/test_cancel_order_service.py
@@ -1,6 +1,7 @@
 import pytest
 from unittest import mock
 from app.order.domain.order_state import OrderState
+from app.order.domain.exceptions import AlreadyShippedException
 from ..cancel_order_service import CancelOrderService
 
 
@@ -18,3 +19,16 @@ def test_cancel(mock_handle, db_session, cancel_order_service, loginned_user, or
     db_session.refresh(order)
     assert order.state == OrderState.CANCELED
     mock_handle.assert_called_with(mock.ANY)
+
+
+def test_cancel_GIVEN_AFTER_SHIPPED_THEN_ERROR(db_session, cancel_order_service, loginned_user, order):
+    # Given
+    after_shipped_state = [OrderState.SHIPPED,
+                           OrderState.DELIVERING, OrderState.DELIVERY_COMPLETED]
+
+    # When
+    for state in after_shipped_state:
+        order.state = state
+        db_session.add(order)
+        with pytest.raises(AlreadyShippedException):
+            cancel_order_service.cancel(order.id, loginned_user)
